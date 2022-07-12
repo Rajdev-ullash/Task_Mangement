b0d8bb54e2675e71e114a5e60ec18aeb3ec590ee
Rajdev-ullash <rajdevullash111@gmail.com>
2022-07-12 13:33:00 +0600

    page routing setup complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,35 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CanceledPage from "./pages/Canceled-Page";
+import CompletedPage from "./pages/Completed-Page";
+import CreatePage from "./pages/Create-Page";
+import DashboardPage from "./pages/Dashboard-Page";
+import ForgetpassPage from "./pages/Forgetpass-Page";
+import LoginPage from "./pages/Login-Page";
+import NewPage from "./pages/New-Page";
+import Page404 from "./pages/Page-404";
+import ProfilePage from "./pages/Profile-Page";
+import ProgressPage from "./pages/Progress-Page";
+import RegistrationPage from "./pages/Registration-Page";
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<DashboardPage />} />
+          <Route exact path="/Create" element={<CreatePage />} />
+          <Route exact path="/All" element={<NewPage />} />
+          <Route exact path="/Progress" element={<ProgressPage />} />
+          <Route exact path="/Completed" element={<CompletedPage />} />
+          <Route exact path="/Canceled" element={<CanceledPage />} />
+          <Route exact path="/Profile" element={<ProfilePage />} />
+          <Route exact path="/Login" element={<LoginPage />} />
+          <Route exact path="/Registration" element={<RegistrationPage />} />
+          <Route exact path="/Forgetpass" element={<ForgetpassPage />} />
+          <Route exact path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
   );
 };
 
--- client/src/pages/Dashboard-Page.jsx
+++ client/src/pages/Dashboard-Page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const DashboardPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+    </div>
+  );
 };
 
 export default DashboardPage;
